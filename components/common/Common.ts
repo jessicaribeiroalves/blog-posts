16c26811cf10c8fdd5a4e28dd16abecb84b72dd4
Jessica Alves <jessralves@gmail.com>
2023-04-04 11:10:53 -0400

    remove common folder

--- components/common/Common.ts
+++
@@ -1,9 +0,0 @@
-// import styled from 'styled-components';
-// import { ThemeProvider } from 'styled-components';
-
-// export const Title = styled.h1`
-// font-size: 1.5em;
-// text-align: center;
-// color: palevioletred;
-// padding: 10px;
-// `;

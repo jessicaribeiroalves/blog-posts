58de78fe2d64c0bef61492eebfc46868cffc3e11
Jessica Alves <jessralves@gmail.com>
2023-04-04 14:18:21 -0400

    remove head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Blog Title</title>
-        <meta name="description" content="Blog description" />
-        <meta property="og:title" content="Blog Title" />
-        <meta property="og:description" content="Blog description" />
-        <meta property="og:URL" content="https://blog.com/" />
-        <meta property="og:type" content="website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
